5c0605cc8ced0c47776e0c4150120bbc738cddd9
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-02 01:36:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(characters.name) FROM characters_books JOIN characters on characters.id = character_books.character_id JOIN books on books.id = character_books.book_id"
+  "SELECT characters.name, COUNT(characters.name) FROM character_books JOIN characters on characters.id = character_books.character_id JOIN books on books.id = character_books.book_id"
 end
